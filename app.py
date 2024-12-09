59533c301a57e9963ed6fe0365778aa2612ab0f2
Haaa012 <154519447+Haaa012@users.noreply.github.com>
2024-12-09 16:42:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from pymongo import MongoClient
 from bson import ObjectId, json_util
 import json
+import os
+
 app = Flask(__name__)
+
 # Connexion à MongoDB
 client = MongoClient("mongodb+srv://haaahiii:1234@cluster1.wmj7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 db = client['Data_ko']
 collection = db['Data_collectio_nama']
+
+@app.route('/')
+def serve_frontend():
+    return send_from_directory('templates', 'index.html')
+
 @app.route('/create', methods=['POST'])
 def create_data():
     data = request.json
     result = collection.insert_one(data)
     return jsonify({"status": "success", "inserted_id": str(result.inserted_id)})
+
 @app.route('/get_data', methods=['GET'])
 def get_data():
     data = list(collection.find())
     json_data = json.loads(json_util.dumps(data))
     return jsonify(json_data)
+
 @app.route('/update/<id>', methods=['PUT'])
 def update_data(id):
     data = request.json
     result = collection.update_one({"_id": ObjectId(id)}, {"$set": data})
     return jsonify({"status": "success", "modified_count": result.modified_count})
+
 @app.route('/delete/<id>', methods=['DELETE'])
 def delete_data(id):
     result = collection.delete_one({"_id": ObjectId(id)})
     return jsonify({"status": "success", "deleted_count": result.deleted_count})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
