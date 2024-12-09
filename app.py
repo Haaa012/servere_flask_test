89a02b49e434048866ccbc30fc3e589154db2bfa
Haaa012 <154519447+Haaa012@users.noreply.github.com>
2024-12-09 09:53:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return render_template("index.html", now=datetime.now())
+@app.route('/')
+def hello():
+    return "Bonjour! Cette application est déployée sur Render."
 
-
-if __name__ == "__main__":
-	app.run()
+if __name__ == '__main__':
+    # Utilisez le port fourni par l'environnement ou 5000 par défaut
+    port = int(os.environ.get('PORT', 5000))
+    # Assurez-vous que l'application écoute sur '0.0.0.0' pour Render
+    app.run(host='0.0.0.0', port=port)
