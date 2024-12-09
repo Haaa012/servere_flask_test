d72579322742cf32a84f1882555ac2c2e2a3dc5f
Haaa012 <154519447+Haaa012@users.noreply.github.com>
2024-12-09 11:00:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson import json_util
+import json
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Bonjour! Cette application est déployée sur Render."
-
+# Connexion à MongoDB
+client = MongoClient("mongodb+srv://haaahiii:1234@cluster1.wmj7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+db = client['Data_ko']
+collection = db['Data_collectio_nama']
+@app.route('/get_data')
+def get_data():
+    # Récupérer les données de MongoDB
+    data = list(collection.find())
+    # Convertir les données en JSON
+    json_data = json.loads(json_util.dumps(data))    
+    # Renvoyer les données au format JSON
+    return jsonify(json_data)
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
