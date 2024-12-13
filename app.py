39c0e1a65f9b4c8b8fb2a76b084a6760a8f34a30
Haaa012 <154519447+Haaa012@users.noreply.github.com>
2024-12-13 16:45:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId, json_util
 import json
-import os
 
 app = Flask(__name__)
 
@@ -10,10 +9,6 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://haaahiii:1234@cluster1.wmj7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 db = client['Data_ko']
 collection = db['Data_collectio_nama']
-
-@app.route('/')
-def serve_frontend():
-    return send_from_directory('templates', 'index.html')
 
 @app.route('/create', methods=['POST'])
 def create_data():
@@ -37,24 +32,28 @@ def update_data(id):
 def delete_data(id):
     result = collection.delete_one({"_id": ObjectId(id)})
     return jsonify({"status": "success", "deleted_count": result.deleted_count})
-@app.route('/authenticate', methods=['POST']) 
-	def authenticate_card(): 
-		data = request.json 
-		card_id = data.get("Carte") 
-		if not card_id: 
-			return jsonify({"authenticated": False, "message": "Aucune carte ID fournie"}), 400 
-		# Rechercher la carte dans la base de données 
-		card = collection.find_one({"Carte": card_id}) 
-		if card: 
-			return jsonify({ 
-				"authenticated": True, 
-				"Nom": card.get("Nom"), 
-				"Carte": card.get("Carte"), 
-				"Compte": card.get("Compte"), 
-				"message": "Carte authentifiée" 
-			}), 200 
-		else: 
-			return jsonify({"authenticated": False, "message": "Carte non authentifiée"}), 404     
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate_card():
+    data = request.json
+    card_id = data.get("Carte")
+
+    if not card_id:
+        return jsonify({"authenticated": False, "message": "Aucune carte ID fournie"}), 400
+
+    # Rechercher la carte dans la base de données
+    card = collection.find_one({"Carte": card_id})
+
+    if card:
+        return jsonify({
+            "authenticated": True,
+            "Nom": card.get("Nom"),
+            "Carte": card.get("Carte"),
+            "Compte": card.get("Compte"),
+            "message": "Carte authentifiée"
+        }), 200
+    else:
+        return jsonify({"authenticated": False, "message": "Carte non authentifiée"}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
